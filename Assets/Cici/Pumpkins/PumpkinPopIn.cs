cb3348dfdb212988f4f8d85cdc5729c4056c4a8a
Christopher Raheb <craheb123@gmail.com>
2024-10-10 15:41:27 -0700

    Added shrinking on pop in

--- Assets/Cici/Pumpkins/PumpkinPopIn.cs
+++ Assets/Cici/Pumpkins/PumpkinPopIn.cs
@@ -9,24 +9,28 @@ public class PumpkinPopIn : MonoBehaviour
 
     SpriteRenderer spriteRenderer;
     bool onScreen = false;
+    Vector3 startScale;
 
     // Start is called before the first frame update
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        startScale = transform.localScale;
     }
 
     void FixedUpdate()
     {
         if (onScreen)
         {
-            //spriteRenderer.color = Color.Lerp(spriteRenderer.color, new Color(1, 1, 1, 1), Mathf.PingPong(Time.fixedTime, 1));
             spriteRenderer.color = Color.Lerp(spriteRenderer.color, new Color(1, 1, 1, 1), fadeTime);
+            transform.localScale = Vector3.Lerp(transform.localScale, startScale, fadeTime / 2);
+            spriteRenderer.sortingLayerName = "Default";
         }
         else
         {
-            //spriteRenderer.color = Color.Lerp(spriteRenderer.color, new Color(1, 1, 1, 0), Mathf.PingPong(Time.fixedTime, 1));
             spriteRenderer.color = Color.Lerp(spriteRenderer.color, new Color(1, 1, 1, 0), fadeTime);
+            transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(0, 0, 0), fadeTime / 2);
+            spriteRenderer.sortingLayerName = "BehindBG";
         }
     }
 
